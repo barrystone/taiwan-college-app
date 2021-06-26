4b5509987683837f373716e8b2a2fb49d5cd1bbb
barrystone <barrystone3183@gmail.com>
2021-06-27 04:12:13 +0800

    [add] google sheet api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [allColleges, setAllColeges] = useState([]);
+  const [searchRow, setSearchRow] = useState(365);
+  useEffect(() => {
+    getCollegeRow();
+    requestCollegeData(searchRow);
+  }, []);
+
+  const requestCollegeData = async (row: number) => {
+    const range = `A${row}:H${row}`;
+    const res = await fetch(
+      `${process.env.REACT_APP_GOOGLE_SHEET_API_BASE}/${process.env.REACT_APP_GOOGLE_SHEET_ID}/values/${range}?key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+    );
+
+    const { values } = await res.json();
+    console.log(values);
+  };
+
+  const getCollegeRow = async () => {
+    const range = `E:E`;
+    const res = await fetch(
+      `${process.env.REACT_APP_GOOGLE_SHEET_API_BASE}/${process.env.REACT_APP_GOOGLE_SHEET_ID}/values/${range}?key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+    );
+
+    const { values } = await res.json();
+    setAllColeges(values);
+  };
+
   return (
     <div className="App">
       <header className="App-header">

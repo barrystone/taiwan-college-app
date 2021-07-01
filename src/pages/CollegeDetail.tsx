c5bb4e4f3bc79eac3a30a3e60e148bb051f7eae5
barrystone <barrystone3183@gmail.com>
2021-07-01 08:39:53 +0800

    [add] basic info display on left part in CollegeDetail page

--- src/pages/CollegeDetail.tsx
+++ src/pages/CollegeDetail.tsx
@@ -1,4 +1,5 @@
-import { Col, Row, Container } from 'react-bootstrap';
+import { relative } from 'path';
+import { Col, Row, Container, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import BackArrowIcon from '../assets/images/back-arrow.svg';
@@ -18,7 +19,12 @@ const CollegeDetail = ({ match: { params }, allColleges }: Props) => {
   const { id } = params;
 
   const collegeData = allColleges.filter((e) => e[3] === id);
-  const latestName = collegeData.slice(-1).map((x) => x[4]);
+  const lastCollegeData = collegeData.slice(-1);
+  const latestName = lastCollegeData.map((x) => x[4])[0];
+  const latestPublicType = lastCollegeData.map((x) => x[1])[0];
+  const latestSchoolType = lastCollegeData.map((x) => x[2])[0];
+  const latestYear = lastCollegeData.map((x) => x[0])[0];
+  const latestRatio = lastCollegeData.map((x) => x[7])[0];
 
   // Calculate average all of students or teacher (country) base on year.
   const clacAvg = (year: string, stuOrTch: string) => {
@@ -39,12 +45,12 @@ const CollegeDetail = ({ match: { params }, allColleges }: Props) => {
     return Avg;
   };
 
-  const studentsAllYearsAvg = [
+  const studentsAllYearsAvg: Array<string> = [
     clacAvg('108', 'stu'),
     clacAvg('107', 'stu'),
     clacAvg('106', 'stu')
   ];
-  const teachersAllYearsAvg = [
+  const teachersAllYearsAvg: Array<string> = [
     clacAvg('108', 'tch'),
     clacAvg('107', 'tch'),
     clacAvg('106', 'tch')
@@ -69,6 +75,10 @@ const CollegeDetail = ({ match: { params }, allColleges }: Props) => {
       }
     ];
   }) as any;
+
+  const StutTch108Ratio = (
+    Number(studentsAllYearsAvg[0]) / Number(teachersAllYearsAvg[0])
+  ).toFixed(2);
 
   return (
     <>
@@ -95,10 +105,104 @@ const CollegeDetail = ({ match: { params }, allColleges }: Props) => {
                 style={{ position: 'absolute', top: '20px', left: '20px' }}
               />
             </Link>
-
-            <div style={{}}>
-              <h1>{latestName}</h1>
-            </div>
+            <Container
+              style={{
+                display: 'grid',
+                flexDirection: 'column',
+                gridTemplateRows: '1fr 1fr 1fr',
+                height: '100%'
+              }}
+            >
+              {/* style={{ backgroundColor: 'red' }} */}
+              <Row
+                style={
+                  {
+                    // backgroundColor: 'yellow'
+                  }
+                }
+              ></Row>
+              <Row
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  position: 'relative'
+                  // backgroundColor: 'red',
+                }}
+              >
+                <div
+                  style={{
+                    position: 'absolute',
+                    left: '25px',
+                    top: '50px'
+                    // height: '400px',
+                    // backgroundColor: 'orange'
+                  }}
+                >
+                  <h4>
+                    {latestSchoolType === '宗教研修學院' ? (
+                      <Badge variant="danger">宗教研修學院</Badge>
+                    ) : latestSchoolType === '技專校院' ? (
+                      <Badge variant="secondary">技專校院</Badge>
+                    ) : (
+                      <Badge variant="primary">一般大學</Badge>
+                    )}{' '}
+                  </h4>
+                  <h4>
+                    {latestPublicType === '私立' ? (
+                      <Badge variant="info">私立</Badge>
+                    ) : (
+                      <Badge variant="success">公立</Badge>
+                    )}
+                  </h4>
+                </div>
+                <h1 style={{ maxWidth: '50%' }}>{latestName}</h1>
+              </Row>
+              <Row
+                style={
+                  {
+                    // backgroundColor: 'green'
+                  }
+                }
+              >
+                <Col
+                  style={{
+                    position: 'relative'
+                    // backgroundColor: 'purple'
+                  }}
+                >
+                  <div style={{ position: 'absolute', left: '10%', top: '0%' }}>
+                    <h5>
+                      <Badge pill variant="danger">
+                        {latestYear}
+                      </Badge>{' '}
+                    </h5>
+                  </div>
+                  <div
+                    style={{ position: 'absolute', left: '15%', top: '20%' }}
+                  >
+                    <h2>
+                      生師比 &nbsp;&nbsp;
+                      <span style={{ fontSize: '40px' }}>
+                        {latestRatio}
+                      </span>{' '}
+                      &nbsp; %
+                    </h2>
+                  </div>
+                  <div
+                    style={{ position: 'absolute', left: '20%', top: '60%' }}
+                  >
+                    <h6>
+                      每校平均生師比 &nbsp;
+                      <span style={{ fontSize: '20px' }}>
+                        {StutTch108Ratio}
+                      </span>
+                      %&nbsp;<span style={{ fontSize: '13px' }}> (108年)</span>
+                    </h6>
+                  </div>
+                </Col>
+                <Col></Col>
+              </Row>
+            </Container>
           </Col>
           <Col
             style={{
